38747e79da26c5738f510160af9c3e284be0a465
neoviews <oluwakayodeasamuel@gmail.com>
2023-01-24 21:50:04 +0100

    First release

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Products from './components/pages/Products'
+import Services from './components/pages/Services'
+import SignUp from './components/pages/SignUp'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Home />}/>
+          <Route path='/products' exact element={<Products />}/>
+          <Route path='/services' exact element={<Services />}/>
+          <Route path='/sign-up' exact element={<SignUp />}/>
+        </Routes>
+      </Router>      
+    </>
+  );
+}
+
+export default App;
